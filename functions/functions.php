208ac2bdec16a63fe823bde74f50086ac4436d18
Bryon Severns <bryon.severns@gmail.com>
2019-12-03 09:07:13 -0600

    .gitignore commit

--- functions/functions.php
+++ functions/functions.php
@@ -15,6 +15,23 @@ function getRealUserIP(){
 // add_cart function starts
 function add_cart(){
     global $db;
+    if(isset($_GET['add_cart'])){
+        $ip_add = getRealUserIp();
+        $p_id = $_GET['add_cart'];
+        $product_qty = $_POST['product_qty'];
+        $product_size = $_POST['product_size'];
+        $check_product = "select * from cart where ip_add = '$ip_add' AND p_id='$p_id'";
+        $run_check = mysqli_query($db, $check_product);
+        if(mysqli_num_rows($run_check)>0){
+            echo "<script>alert('This product is already in cart')</script>";
+            echo "<script>window.open('details.php?pro_id=$p_id,'_self')</script>";
+        }else{
+            $query = "insert into cart (p_id,ip_add,qty,size) values ('$p_id','$ip_add','$product_qty','$product_size')";
+            $run_query = mysqli_query($db, $query);
+            echo "<script>window.open('details.php?pro_id=$p_id,'_self')</script>";
+        }
+            
+    }
     
 }// add_cart function ends
 
