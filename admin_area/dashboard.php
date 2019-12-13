4aec69e4651cb842591c175e169a34527817da44
Bryon Severns <bryon.severns@gmail.com>
2019-12-12 20:47:03 -0600

    .gitignore commit

---
+++ admin_area/dashboard.php
@@ -0,0 +1,13 @@
+
+<div class="row"><!--1 row starts-->
+    <div class="col-md-2" ></div>
+    <div class="col-lg-10"><!--col-lg-12 starts-->
+        <h1 class="page-header">Dashboard</h1>
+        <ol class="breadcrumb"><!--breadcrumb starts-->
+            <li class="active">
+                <i class="fa fa-dashboard"></i>Dashboard
+            </li>
+        </ol><!--breadcrumb ends-->
+    </div><!--col-lg-12 ends-->
+</div><!--1 row ends-->
+
