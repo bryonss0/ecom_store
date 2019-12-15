9699d7c7f0be05eb9079cab5cae5b4a165085f45
Bryon Severns <bryon.severns@gmail.com>
2019-12-14 22:25:13 -0600

    .gitignore commit

--- admin_area/dashboard.php
+++ admin_area/dashboard.php
@@ -1,13 +1,109 @@
 
 <div class="row"><!--1 row starts-->
-    <div class="col-md-2" ></div>
-    <div class="col-lg-10"><!--col-lg-12 starts-->
+    <div class="col-md-2"></div><!--because offset-md-2 didn't work-->
+    <div class="col-md-10 offset-md-2"><!--col-md-10 starts-->
         <h1 class="page-header">Dashboard</h1>
         <ol class="breadcrumb"><!--breadcrumb starts-->
             <li class="active">
                 <i class="fa fa-dashboard"></i>Dashboard
             </li>
         </ol><!--breadcrumb ends-->
-    </div><!--col-lg-12 ends-->
+    </div><!--col-md-10 ends-->
 </div><!--1 row ends-->
 
+<div class="row"><!--2 row starts-->
+    <div class="col-md-2"></div><!--because offset-md-2 didn't work-->
+    <div class="col-md-2 "><!--col-lg-3 col-md-6 starts-->
+        <div class="panel panel-primary"><!--panel panel-primary starts-->
+            <div class="panel-heading"><!--panel-heading starts-->
+                <div class="row"><!--panel-heading row starts-->
+                    <div class="col-xs-3"><!--col-xs-3 starts-->
+                        <i class="fa fa-tasks fa-5x"></i>
+                    </div><!--col-xs-3 ends-->
+                    <div class="col-xs-9 text-right"><!--col-xs-9-text-right starts-->
+                        <div class="huge"> 15 </div>
+                        <div>Products</div>
+                    </div><!--col-xs-9-text-right ends-->
+                </div><!--panel-heading row ends-->
+            </div><!--panel-heading ends-->
+            <a href="index.php?view_products">
+                <div class="panel-footer"><!--panel-footer starts-->
+                    <span class="pull-left">View Details</span>
+                    <span class="pull-right"><i class="fa fa-arrow-circle-right"></i></span>
+                    <div class="clearfix"></div>
+                </div><!--panel-footer ends-->               
+            </a>
+        </div><!--panel panel-primary ends-->
+    </div> <!--col-lg-3 col-md-6 ends-->
+    
+        <div class="col-md-2"><!--col-lg-3 col-md-6 starts-->
+        <div class="panel panel-primary"><!--panel panel-primary starts-->
+            <div class="panel-heading"><!--panel-heading starts-->
+                <div class="row"><!--panel-heading row starts-->
+                    <div class="col-xs-3"><!--col-xs-3 starts-->
+                        <i class="fa fa-tasks fa-5x"></i>
+                    </div><!--col-xs-3 ends-->
+                    <div class="col-xs-9 text-right"><!--col-xs-9-text-right starts-->
+                        <div class="huge"> 15 </div>
+                        <div>Products</div>
+                    </div><!--col-xs-9-text-right ends-->
+                </div><!--panel-heading row ends-->
+            </div><!--panel-heading ends-->
+            <a href="index.php?view_products">
+                <div class="panel-footer"><!--panel-footer starts-->
+                    <span class="pull-left">View Details</span>
+                    <span class="pull-right"><i class="fa fa-arrow-circle-right"></i></span>
+                    <div class="clearfix"></div>
+                </div><!--panel-footer ends-->               
+            </a>
+        </div><!--panel panel-primary ends-->
+    </div> <!--col-lg-3 col-md-6 ends-->
+    
+        <div class="col-md-2"><!--col-lg-3 col-md-6 starts-->
+        <div class="panel panel-primary"><!--panel panel-primary starts-->
+            <div class="panel-heading"><!--panel-heading starts-->
+                <div class="row"><!--panel-heading row starts-->
+                    <div class="col-xs-3"><!--col-xs-3 starts-->
+                        <i class="fa fa-tasks fa-5x"></i>
+                    </div><!--col-xs-3 ends-->
+                    <div class="col-xs-9 text-right"><!--col-xs-9-text-right starts-->
+                        <div class="huge"> 15 </div>
+                        <div>Products</div>
+                    </div><!--col-xs-9-text-right ends-->
+                </div><!--panel-heading row ends-->
+            </div><!--panel-heading ends-->
+            <a href="index.php?view_products">
+                <div class="panel-footer"><!--panel-footer starts-->
+                    <span class="pull-left">View Details</span>
+                    <span class="pull-right"><i class="fa fa-arrow-circle-right"></i></span>
+                    <div class="clearfix"></div>
+                </div><!--panel-footer ends-->               
+            </a>
+        </div><!--panel panel-primary ends-->
+    </div> <!--col-lg-3 col-md-6 ends-->
+    
+        <div class="col-md-2"><!--col-lg-3 col-md-6 starts-->
+        <div class="panel panel-primary"><!--panel panel-primary starts-->
+            <div class="panel-heading"><!--panel-heading starts-->
+                <div class="row"><!--panel-heading row starts-->
+                    <div class="col-xs-3"><!--col-xs-3 starts-->
+                        <i class="fa fa-tasks fa-5x"></i>
+                    </div><!--col-xs-3 ends-->
+                    <div class="col-xs-9 text-right"><!--col-xs-9-text-right starts-->
+                        <div class="huge"> 15 </div>
+                        <div>Products</div>
+                    </div><!--col-xs-9-text-right ends-->
+                </div><!--panel-heading row ends-->
+            </div><!--panel-heading ends-->
+            <a href="index.php?view_products">
+                <div class="panel-footer"><!--panel-footer starts-->
+                    <span class="pull-left">View Details</span>
+                    <span class="pull-right"><i class="fa fa-arrow-circle-right"></i></span>
+                    <div class="clearfix"></div>
+                </div><!--panel-footer ends-->               
+            </a>
+        </div><!--panel panel-primary ends-->
+    </div> <!--col-lg-3 col-md-6 ends-->
+    
+    
+</div><!--2 row ends-->
