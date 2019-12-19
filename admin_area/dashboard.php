bdaa84889f8b7c7a23bda7e4547d9378c58501f7
Bryon Severns <bryon.severns@gmail.com>
2019-12-18 20:15:59 -0600

    .gitignore commit

--- admin_area/dashboard.php
+++ admin_area/dashboard.php
@@ -108,13 +108,97 @@
 
 <div class="row"><!-- 3 row starts-->
     <div class="col-md-2"></div><!--because offset-md-2 didn't work-->
-    <div class="col-lg-8"><!--col-lg-8 starts-->
+    <div class="col-lg-6"><!--col-lg-8 starts-->
         <div class="panel panel-primary"><!--panel panel-primary starts-->
             <div class="panel-heading"><!--panel-heading starts-->
                 <h3 class="panel-title"><!--panel-title starts-->
                     <i class="fa fa-money fa-fw"></i>New Orders
                 </h3><!--panel-title ends-->
             </div><!--panel-heading starts-->
+            <div class="panel-body"><!--panel-body starts-->
+                <div class="table-responsive"><!--table-responsive starts-->
+                    <table class="table table-bordered table-hover table-striped"><!--table table-bordered table-hover table-striped starts-->
+                        <thead><!--thead starts-->
+                            <tr>
+                                <th>Order No:</th>
+                                <th>Customer Email:</th>
+                                <th>Invoice No:</th>
+                                <th>Product ID:</th>
+                                <th>Product Qty:</th>
+                                <th>Product Size:</th>
+                                <th>Status:</th>                      
+                            </tr>
+                        </thead><!--thead ends-->
+                        <btody><!--tbody starts-->
+                            <tr>
+                                <td>1</dt>
+                                <td>brock@gmail.com</dt>
+                                <td>78055</dt>
+                                <td>32</dt>
+                                <td>2</dt> 
+                                <td>Large</dt>
+                                <td>Complete</dt>    
+                            </tr>
+                            <tr>
+                                <td>1</dt>
+                                <td>brock@gmail.com</dt>
+                                <td>78055</dt>
+                                <td>32</dt>
+                                <td>2</dt> 
+                                <td>Large</dt>
+                                <td>Complete</dt>    
+                            </tr>
+                            <tr>
+                                <td>1</dt>
+                                <td>brock@gmail.com</dt>
+                                <td>78055</dt>
+                                <td>32</dt>
+                                <td>2</dt> 
+                                <td>Large</dt>
+                                <td>Complete</dt>    
+                            </tr>
+                            <tr>
+                                <td>1</dt>
+                                <td>brock@gmail.com</dt>
+                                <td>78055</dt>
+                                <td>32</dt>
+                                <td>2</dt> 
+                                <td>Large</dt>
+                                <td>Complete</dt>    
+                            </tr>
+                        </btody><!--tbody ends-->
+                    </table><!--table table-bordered table-hover table-striped ends-->
+                </div><!--table-responsive ends-->
+                <div class="text-right"><!--text-right starts-->
+                    <a href="index.php?view_orders">
+                        View All Orders<i class='fa fa-arrow-circle-right'></i>                       
+                    </a>
+                </div><!--text-right ends-->
+            </div><!--panel-body ends-->            
         </div><!--panel panel-primary ends-->
     </div><!--col-lg-8 ends-->
+    
+    <div class='col-md-4'><!--col-md-4 starts-->
+        <div class='panel'><!--panel starts-->
+            <div class="panel-body"><!--panel-body starts-->
+                <div class="thumb-info mb-md"><!--thumb-info mb-md starts-->
+                    <img src="admin_images/sohail.jpg" class="rounded img-responsive">
+                    <div class="thumb-info-title"><!--thumb-info-title starts-->
+                        <span class="thumb-info-inner">Haris Sohail</span>
+                        <span class="thumb-info-type"> Cricket Player</span>
+                    </div><!--thumb-info-title ends-->
+                </div><!--thumb-info mb-md ends-->
+                <div class="mb-md"><!--mb-md starts-->
+                    <div class="widget-content-expanded"><!--widget-content-expanded starts-->
+                        <i class="fa fa-user"></i><span>Email: </span>sohail@gmail.com <br>
+                        <i class="fa fa-user"></i><span>Country: </span>Pakistan <br>
+                        <i class="fa fa-user"></i><span>Phone: </span>0334678931 <br>
+                    </div><!--widget-content-expanded ends-->
+                    <hr class="dotted short">
+                    <h5 class="text-muted">About</h5>
+                    <p>Haris Sohail is a champion Cricket player in Pakistan. He loves winning and wants all of his fans to be winners. If you want to be a winner, you can follow his example and make a habit of winning.</p>
+                </div><!--mb-md ends-->
+            </div><!--panel-body ends-->
+        </div><!--panel ends-->
+    </div><!--col-md-4 ends-->
 </div><!-- 3 row ends-->
